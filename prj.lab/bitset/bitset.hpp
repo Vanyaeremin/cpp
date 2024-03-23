477b61faa2a1c4f475a526cc2e0704a0e9794d0a
Ivan Eremin <vanyusha_eremin.06@mail.ru>
2024-03-23 20:16:59 +0300

    New class BitSet

---
+++ prj.lab/bitset/bitset.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef BITSET_HPP
+#define BITSET_HPP
+
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+
+class BitSet {
+public:
+    BitSet() = default;
+    BitSet(const BitSet& rhs) = default;
+    BitSet(BitSet&& rhs) = default;
+    explicit BitSet(const std::int32_t size): size_(size), bits_(size) {}
+    BitSet& operator=(const BitSet& rhs) = default;
+    BitSet& operator=(BitSet&& rhs) = default;
+    ~BitSet() = default;
+
+    [[nodiscard]] bool operator==(const BitSet& rhs) const noexcept;
+    [[nodiscard]] bool operator!=(const BitSet& rhs) const noexcept;
+    std::int32_t Size() const noexcept { return size_; }
+    void Resize(const std::int32_t size);
+    [[nodiscard]] bool Get(const int32_t idx) const;
+    void Set(const int32_t idx, const bool val);
+    void Fill(const bool val) noexcept;
+
+    [[nodiscard]] BitSet& operator~();
+    [[nodiscard]] BitSet& operator|(const BitSet& rhs);
+    [[nodiscard]] BitSet& operator&(const BitSet& rhs);
+    [[nodiscard]] BitSet& operator^(const BitSet& rhs);
+    std::vector<std::uint32_t> same_size(const BitSet& v1, const BitSet& v2);
+
+private:
+    std::int32_t size_ = 0;
+    std::vector<std::uint32_t> bits_;
+};
+
+#endif
